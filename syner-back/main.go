8c96c247702e7bc01832c16155aa9802a817d656
Максим <panda5156@mail.ru>
2025-06-04 15:19:53 +0300

    new

---
+++ syner-back/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"net/http"
+	"os"
+
+	"github.com/labstack/echo/v4"
+)
+
+func main() {
+	e := echo.New()
+
+	e.GET("/ping", func(c echo.Context) error {
+		return c.String(http.StatusOK, "pong")
+	})
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	e.Logger.Fatal(e.Start(":" + port))
+}
